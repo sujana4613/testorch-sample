2dc300c28a208609b90044620e16a8fb2d9e7ad0
sujana <sujana4613@gmail.com>
2024-07-13 16:43:04 +0530

    testing

---
+++ src/test/java/UI/sampleclass.java
@@ -0,0 +1,20 @@
+package UI;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class sampleclass {
+
+	@Test
+	public void test1()
+	{
+		Assert.assertEquals("1", "1");
+	}
+	
+	@Test
+	public void test2()
+	{
+		Assert.assertEquals("1", "1");
+	}
+
+}
